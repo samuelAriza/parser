a87ceb3a4388a8d0588e44004870db410398ddec
Samuel Ariza <samargo0317@gmail.com>
2023-11-02 21:41:09 -0500

    New structure. Missing to add restrictions regarding when rejecting strings, in general, works.

--- parser.py
+++ parser.py
@@ -1,74 +1,61 @@
-rules = ["S-aSb", "S-c"]
+rules = ["S-aSb","S-c"]
 nt = ["S"]
-string = "acb"
+string = "a$"
 c = 0
-a = string[c]
-flag = True
+a = string[0]
+flag = False
+devolver = False
+k = 0
 
-
-def set_parts(N):
-    right = []
+def productions(N):
+    productions = []
     for i in range(0, len(rules)):
         if rules[i][0] == N:
-            right.append((rules[i].split("-"))[1])
-    return right
-
-it = 0
-
-def validation(flag, N):
-    global c
-    global a
-    global it
-
-    parts = set_parts(N)
-    if flag == True:
-        A = parts[it]
-        return A
-    elif flag == False:
-        if it >= len(parts) - 1:
-            c = c - 1
-            a = string[c]
-        else:
-            it = it + 1
-            A = parts[it]
-            return A
+            productions.append((rules[i].split("-"))[1])
+    return productions
 
 def recursive_parser(N):
-    global a 
     global c
-    global flag
-    
-    A = validation(flag, N)
-    print(f'Ahora la regla es {A}')
-    print(f'Let aaaa = {a}')
-    print(f'El {A == None}')
-    if A == None:
-        return "no"
+    global string
+    global a 
+    global k 
+    flag = False
+    global devolver
+    listo = False
 
-    for i in range(0, len(A)):
-        print(f'Iteracion {i}')
-        if A[i] in nt:
-            print("Es un no terminal")
-            print(f'Xi = {A[i]}')
-            recursive_parser(A[i])
-        elif A[i] == a:
-            print("Coincide:")
-            print(f'let a = {a}')
-            c = c + 1
-            try:
+    parts = productions(N)
+    for i in range(0, len(parts)):
+        print(f'Estoy en la iteracion {i}')
+        A = parts[i]
+        print(f'La produccion es {A}')
+        for j in range(0, len(A)):
+            print(A[j])
+            if A[j] in nt:
+                print(f'{A[j]} es un no terminal')
+                recursive_parser(A[j])
+            elif A[j] == a:
+                print(f'{A[j]} es igual let a = {a} ')
+                c = c + 1
                 a = string[c]
-            except:
-                return "yes"
-            print(f'Nuevo {a}')
-            flag = True
-        else:
-            print("No es la regla de produccion")
-            flag = False
-            recursive_parser(N)
+                k = k + 1
+                flag = True
+                if j == len(A) - 1:
+                    print("piki")
+                    listo = True
+                    break
+            else:
+                print(f'{A[j]} no era la regla de produccion')
+                if devolver == True:
+                    print("Hice un break")
+                    devolver = False
+                break
+        if listo == True:
             break
-
-x = recursive_parser("S")
-if x == "yes":
-    print("yes")
-elif x == None:
-    print("no")
+    if flag == False and devolver == False:
+        print("Hola")
+        c = c - 1
+        a = string[c]
+        k = k - 1
+        devolver = True
+    
+recursive_parser("S")
